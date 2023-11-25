7e7a4cd08d548b82d91b473650148be336581afb
Jazon Younge <jay@Jazons-MacBook-Air.local>
2023-11-25 13:31:13 -0500

    Initial commit. Bare bones established

---
+++ helpers.js
@@ -0,0 +1,32 @@
+const apiData = fetch("https://www.freetogame.com/api/games")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    console.error("Fetch Error:", error);
+  });
+
+apiData.then((gameData) => {
+  const min = 0;
+  const max = gameData.length - 1;
+  const gamesArr = document.querySelector(".games");
+  for (let i = 0; i < 5; i++) {
+    const gameIndex = Math.round(Math.random() * (max - min + 1) + min);
+
+    gamesArr.innerHTML += `<li>
+        <a href="${gameData[gameIndex].game_url}" target="_blank">
+      <img src="${gameData[gameIndex].thumbnail}" alt="${gameData[gameIndex].title}"/>
+      <p class="cardTitle">Title : ${gameData[gameIndex].title}</p>
+      <p>Genre : ${gameData[gameIndex].genre}</p>
+      <p>Platform : ${gameData[gameIndex].platform}</p>
+      <p>Publisher : ${gameData[gameIndex].publisher}</p>
+      <p>Developer : ${gameData[gameIndex].developer}</p>
+      <p>Release Date : ${gameData[gameIndex].release_date}</p>
+      <p>Description : ${gameData[gameIndex].short_description}</p>
+      </a>;
+  </li>`;
+  }
+});
